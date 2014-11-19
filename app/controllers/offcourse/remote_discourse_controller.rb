d7d003ede91c4c8e4a951730d0db16cf597dfcbc
Ed Tewiah <etewiah@hotmail.cim>
2014-11-19 12:46:36 +0000

    Add endpoint for retrieving topics per category

--- app/controllers/offcourse/remote_discourse_controller.rb
+++ app/controllers/offcourse/remote_discourse_controller.rb
@@ -22,5 +22,50 @@ module Offcourse
         return render json: {"error" => {"message" => "sorry, there has been an error"}}
       end
     end
+
+    def topics_per_category
+      unless(params[:host] && params[:category] )
+        return render json: {"error" => {"message" => "sorry, there has been an error"}}
+      end
+
+      conn = Faraday.new(:url => params[:host]) do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+
+      ## GET ##
+
+      response = conn.get '/c/' + params[:category] + '.json'     # GET http://sushi.com/nigiri/sake.json
+      rb = response.body
+      if response.status == 200
+        return render json: response.body      
+      else
+        return render json: {"error" => {"message" => "sorry, there has been an error"}}
+      end
+    end
+
+    def topic_details
+      unless(params[:host] && params[:topic_id] )
+        return render json: {"error" => {"message" => "sorry, there has been an error"}}
+      end
+
+      conn = Faraday.new(:url => params[:host]) do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+
+      ## GET ##
+
+      response = conn.get '/t/' + params[:topic_id] + '.json'     # GET http://sushi.com/nigiri/sake.json
+      rb = response.body
+      if response.status == 200
+        return render json: response.body      
+      else
+        return render json: {"error" => {"message" => "sorry, there has been an error"}}
+      end
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Offcourse::Engine.routes.draw do
   get "/offcourse/topics" => "offcourse#offline"
   get "/offcourse/topics/*path" => "offcourse#offline"
   get "/remote_discourse/categories" => "remote_discourse#categories"
+  get "/remote_discourse/topics_per_category" => "remote_discourse#topics_per_category"
+  get "/remote_discourse/topic_details" => "remote_discourse#topic_details"
 end
